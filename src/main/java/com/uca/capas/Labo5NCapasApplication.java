012bf2f32c702fa50f22745b1954aaad98693ee2
Fredy Sánchez <00082817@uca.edu.sv>
2020-06-16 13:23:35 -0600

    Aplicacion hasta Laboratorio 6

---
+++ src/main/java/com/uca/capas/Labo5NCapasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labo5NCapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labo5NCapasApplication.class, args);
+	}
+
+}
