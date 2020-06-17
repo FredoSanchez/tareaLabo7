a1738a2b44e3577404241c8f115ecd929c584959
Fredy Sánchez <00082817@uca.edu.sv>
2020-06-16 18:08:12 -0600

    Tarea solucionada, actualizar estudiante

--- src/main/java/com/uca/capas/controller/MainController.java
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -136,4 +137,17 @@ public class MainController {
 		
 		return mav;
 	}
+	
+	
+	@RequestMapping("/update/{id}")
+	public ModelAndView update(@PathVariable(name= "id") int id) {
+		ModelAndView mav = new ModelAndView();
+		Estudiante estudiante = estudianteService.findOne(id);
+		mav.addObject("estudiante", estudiante);
+		mav.setViewName("actualizarEstudiante");
+		
+		return mav;
+	}
+	
+	
 }
